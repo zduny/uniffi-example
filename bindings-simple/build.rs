50d4596755e3e68c5224b28e0233d0748492395f
Daniel Zduniak <dzduniak@gmail.com>
2023-03-02 13:17:35 +0100

    Better error message.

--- bindings-simple/build.rs
+++ bindings-simple/build.rs
@@ -1,5 +1,5 @@
-use std::io::Write;
 use std::fs::{read_to_string, OpenOptions};
+use std::io::Write;
 
 fn main() {
     println!("cargo:rerun-if-changed=src/udl");
@@ -20,8 +20,9 @@ fn join_udl_files(paths: &[&str], output: &str) {
     let udl_file_content = paths
         .iter()
         .map(|path| {
-            println!("{path}");
-            read_to_string(path).unwrap()
+            read_to_string(path).unwrap_or_else(|error| {
+                panic!("error occurred while trying to read \"{path}\": {error}")
+            })
         })
         .collect::<Vec<String>>()
         .join("\n");
